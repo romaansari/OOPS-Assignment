b1b861da822d699395fee892c46b9633bc6e93fb
romaansari <romaansari.2417@gmail.com>
2026-04-01 00:00:27 +0530

    Create Question7(unit3).java

---
+++ Question7(unit3).java
@@ -0,0 +1,31 @@
+class Rectangle {
+    int length, breadth;
+
+    Rectangle(aclass TextEditorBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.append(" World");
+        sb.insert(5, " Java");
+
+        sb.reverse();
+
+        System.out.println("Final String: " + sb);
+    }
+}int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    int Area() {
+        return length * breadth;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4, 5);
+        Rectangle r2 = new Rectangle(5, 8);
+
+        System.out.println("Area of Rectangle 1: " + r1.Area());
+        System.out.println("Area of Rectangle 2: " + r2.Area());
+    }
+}
